5933aa736343c422650ecd2521d701c1d82feb89
unknown <2353231116@qq.com>
2018-05-11 01:16:51 +0800

    commit

---
+++ common/test2.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+a=[1,2,7,8,9,8]
+b=[1,2,4,8,8,8]
+a1=[1,2,7,8,9,8]
+b1=[1,2,7,8,9,8]
+
+for i in range(len(a)):
+    if a[i]==b[i]  a1[i]==b1[i]:
+        pass
+    else:
+        print('no编号是：',i,'a元素是：',a[i],'b元素是：',b[i])
+
+
